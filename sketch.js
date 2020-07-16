78be2a433edd72ddea23689766c5e24d8adbfcd1
TheSnickersBar <39471794+TheSnickersBar@users.noreply.github.com>
2020-07-15 20:08:12 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,15 +16,15 @@ function draw() {
   stroke(255, 100, 150);
   noFill();
   let secondAngle = map(sc, 0, 60, 0, 360);
-  //arc(0, 0, 300, 300, 0, secondAngle);
+  arc(0, 0, 300, 300, 0, secondAngle);
 
   stroke(150, 100, 255);
   let minuteAngle = map(mn, 0, 60, 0, 360);
-  //arc(0, 0, 280, 280, 0, minuteAngle);
+  arc(0, 0, 280, 280, 0, minuteAngle);
 
   stroke(150, 255, 100);
   let hourAngle = map(hr % 12, 0, 12, 0, 360);
-  //arc(0, 0, 260, 260, 0, hourAngle);
+  arc(0, 0, 260, 260, 0, hourAngle);
 
   push();
   rotate(secondAngle);
